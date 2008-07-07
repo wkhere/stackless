f85de5c3441be8d075c4fbffcb311789515b117d
richard.tew <richard.tew@6015fed2-1504-0410-9fe1-9d1591cc4771>
2008-07-07 18:58:49 +0000

    Distutils could handle finding the Stackless include directory, if Stackless had been installed.  However, it could not handle finding the include directory of an uninstalled build.  The latter is taken in preference of the former now.
    git-svn-id: http://svn.python.org/projects/stackless/trunk@64786 6015fed2-1504-0410-9fe1-9d1591cc4771

--- Lib/distutils/command/build_ext.py
+++ Lib/distutils/command/build_ext.py
@@ -161,9 +161,15 @@ class build_ext (Command):
         if plat_py_include != py_include:
             self.include_dirs.append(plat_py_include)
 
-        slpy_include = os.path.join(py_include, "Stackless")
+        # Assume we are a build and not installed first.
+        slpy_include = os.path.join(sysconfig.PREFIX, "Stackless")
         if os.path.exists(slpy_include):
             self.include_dirs.append(slpy_include)
+        else:
+            # Fall back on the installed include directory.
+            slpy_include = os.path.join(py_include, "Stackless")
+            if os.path.exists(slpy_include):
+                self.include_dirs.append(slpy_include)
 
         if type(self.libraries) is StringType:
             self.libraries = [self.libraries]
