dbe59eaeedd4d4cbcc8eec2307aa6ae60d97baaf
kristjan.jonsson <kristjan.jonsson@6015fed2-1504-0410-9fe1-9d1591cc4771>
2008-07-11 15:40:43 +0000

    Add a unitest for proper thread state clearing between tasklets
    git-svn-id: http://svn.python.org/projects/stackless/branches/py3k@64875 6015fed2-1504-0410-9fe1-9d1591cc4771

---
+++ Stackless/unittests/test_tstate.py
@@ -0,0 +1,82 @@
+import unittest
+import sys
+
+from stackless import *
+
+#test that thread state is restored properly
+
+class TestExceptionState(unittest.TestCase):
+    def Tasklet(self):
+        try:
+            1/0
+        except Exception as e:
+            ei = sys.exc_info()
+            print(ei)
+            self.assertEquals(ei[0], ZeroDivisionError)
+            schedule()
+            ei = sys.exc_info()
+            self.assertEquals(ei[0], ZeroDivisionError)
+            self.failUnless("by zero" in str(ei[1]))
+
+    def testExceptionState(self):
+        t = tasklet(self.Tasklet)
+        t()
+        t.run()
+        ei = sys.exc_info()
+        self.assertEquals(ei, (None,)*3)
+        t.run()
+        ei = sys.exc_info()
+        self.assertEquals(ei, (None,)*3)
+
+class TestTracingState(unittest.TestCase):
+    def __init__(self, *args):
+        unittest.TestCase.__init__(self, *args)
+        self.trace = []
+
+    def Callback(self, *args):
+        self.trace.append(args)
+
+    def foo(self):
+        pass
+
+    def Tasklet(self):
+        sys.setprofile(self.Callback)
+
+        self.foo()
+        n = len(self.trace)
+        self.foo()
+        n2 = len(self.trace)
+        self.failUnless(n2 > n)
+
+        schedule()
+
+        self.foo()
+        n = len(self.trace)
+        self.foo()
+        n2 = len(self.trace)
+        self.failUnless(n2 > n)
+
+    def testTracingState(self):
+        t = tasklet(self.Tasklet)
+        t()
+        t.run()
+
+        self.foo()
+        n = len(self.trace)
+        self.foo()
+        n2 = len(self.trace)
+        self.assertEqual(n, n2)
+
+        t.run()
+
+        self.foo()
+        n = len(self.trace)
+        self.foo()
+        n2 = len(self.trace)
+        self.assertEqual(n, n2)
+
+if __name__ == '__main__':
+    import sys
+    if not sys.argv[1:]:
+        sys.argv.append('-v')
+    unittest.main()
