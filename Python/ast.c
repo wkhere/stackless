7fa9a889e59fb1d1e257ad52632e4d6d3f0a836c
kristjan.jonsson <kristjan.jonsson@6015fed2-1504-0410-9fe1-9d1591cc4771>
2008-11-13 16:51:12 +0000

    Merging revisions 66714-67213 of svn+ssh://pythondev@svn.python.org/python/trunk into D:\pydev\stackless\trunk, respecting ancestry This brings stackless/trunk in line with python/trunk and the version level to 2.7
    git-svn-id: http://svn.python.org/projects/stackless/trunk@67214 6015fed2-1504-0410-9fe1-9d1591cc4771

--- Python/ast.c
+++ Python/ast.c
@@ -130,10 +130,17 @@ static int
 forbidden_check(struct compiling *c, const node *n, const char *x)
 {
     if (!strcmp(x, "None"))
-        return ast_error(n, "assignment to None");
-    if (Py_Py3kWarningFlag && !(strcmp(x, "True") && strcmp(x, "False")) &&
-        !ast_warn(c, n, "assignment to True or False is forbidden in 3.x"))
-        return 0;
+        return ast_error(n, "cannot assign to None");
+    if (!strcmp(x, "__debug__"))
+        return ast_error(n, "cannot assign to __debug__");
+    if (Py_Py3kWarningFlag) {
+        if (!(strcmp(x, "True") && strcmp(x, "False")) &&
+            !ast_warn(c, n, "assignment to True or False is forbidden in 3.x"))
+            return 0;
+        if (!strcmp(x, "nonlocal") &&
+            !ast_warn(c, n, "nonlocal is a keyword in 3.x"))
+            return 0;
+    }
     return 1;
 }
 
