56104448060f6fec3529849b651d9e94ce6e6a64
richard.tew <richard.tew@6015fed2-1504-0410-9fe1-9d1591cc4771>
2009-02-24 01:58:05 +0000

    Fix for strange application restarting bug, where in some circumstances an exception was compared to SystemExit, and if it matched, the application was asked to exit.  TaskletExit is a subclass of SystemExit, which means that when it is raised, if it hits this execution path, the restarting occurs.
    Reported by Chris Jacobson.
    
    git-svn-id: http://svn.python.org/projects/stackless/branches/release26-maint@69921 6015fed2-1504-0410-9fe1-9d1591cc4771

--- Python/pythonrun.c
+++ Python/pythonrun.c
@@ -1141,7 +1141,7 @@ PyErr_PrintEx(int set_sys_last_vars)
 {
 	PyObject *exception, *v, *tb, *hook;
 
-	if (PyErr_ExceptionMatches(PyExc_SystemExit)) {
+	if (PyErr_ExceptionMatches(PyExc_SystemExit) && !PyErr_ExceptionMatches(PyExc_TaskletExit)) {
 		handle_system_exit();
 	}
 	PyErr_Fetch(&exception, &v, &tb);
@@ -1163,7 +1163,7 @@ PyErr_PrintEx(int set_sys_last_vars)
 		PyObject *result = PyEval_CallObject(hook, args);
 		if (result == NULL) {
 			PyObject *exception2, *v2, *tb2;
-			if (PyErr_ExceptionMatches(PyExc_SystemExit)) {
+			if (PyErr_ExceptionMatches(PyExc_SystemExit) && !PyErr_ExceptionMatches(PyExc_TaskletExit)) {
 				handle_system_exit();
 			}
 			PyErr_Fetch(&exception2, &v2, &tb2);
