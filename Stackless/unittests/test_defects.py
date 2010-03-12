44ea25cf3781cf3df64e6cbe01ab58c1fdf2288e
kristjan.jonsson <kristjan.jonsson@6015fed2-1504-0410-9fe1-9d1591cc4771>
2010-03-12 11:55:11 +0000

    Merged revisions 76512,77611-77613,77620-77621,78856-78857 via svnmerge from svn+ssh://pythondev@svn.python.org/stackless/trunk
    ................
      r76512 | kristjan.jonsson | 2009-11-25 17:06:59 +0000 (miâ‰¡., 25 nâ‰¤v. 2009) | 1 line
      
      Get rid of the proprietary ZAP macros and use Py_CLEAR instead.
    ................
      r77611 | kristjan.jonsson | 2010-01-19 22:33:25 +0000 (â– ri., 19 jan. 2010) | 2 lines
      
      Adding a module to regression test various defects.
      Initial version contains a disabled test for a problem with __del__ in tasklet main function return values.
    ................
      r77612 | kristjan.jonsson | 2010-01-19 23:10:33 +0000 (â– ri., 19 jan. 2010) | 1 line
      
      Fix a problem where a __del__ statement could cause stackless protocol violations.
    ................
      r77613 | kristjan.jonsson | 2010-01-20 13:13:17 +0000 (miâ‰¡., 20 jan. 2010) | 1 line
      
      Fix the last fix.  Also make sure that a tasklet's __del__ method doesn't cause a crash.
    ................
      r77620 | kristjan.jonsson | 2010-01-21 10:29:49 +0000 (fim., 21 jan. 2010) | 1 line
      
      Adding another disabled unittest for a defect:  stackless.schedule_remove() doesn't work if it is the only runnable tasklet.
    ................
      r77621 | kristjan.jonsson | 2010-01-21 11:05:52 +0000 (fim., 21 jan. 2010) | 1 line
      
      Fix problems with stackless.schedule_remove() when the current tasklet is the last runnable tasklet (it would behave as stackless.schedule())
    ................
      r78856 | kristjan.jonsson | 2010-03-12 09:24:41 +0000 (fÃ·s., 12 mar. 2010) | 3 lines
      
      Initialized merge tracking via "svnmerge" with revisions "1-78853" from 
      svn+ssh://pythondev@svn.python.org/stackless/branches/release26-maint
    ................
      r78857 | kristjan.jonsson | 2010-03-12 09:28:30 +0000 (fÃ·s., 12 mar. 2010) | 11 lines
      
      Merged revisions 78854 via svnmerge from 
      svn+ssh://pythondev@svn.python.org/stackless/branches/release26-maint
      
      ........
        r78854 | kristjan.jonsson | 2010-03-12 08:29:59 +0000 (fâ”œâ•–s., 12 mar. 2010) | 3 lines
        
        Two fixes to stack spilling in stackless:
        1) for low values of CSTACK_WATERMARK, stack spilling could be triggered before stackless initialization.  Ignore those cases
        2) When transfering back from the spilled stack to the original stack, be sure to preserve the current tasklet's tempval.  This may contain the send value on a channel send(), for example and must not be lcobbered.
      ........
    ................
    
    
    git-svn-id: http://svn.python.org/projects/stackless/branches/py3k@78865 6015fed2-1504-0410-9fe1-9d1591cc4771

---
+++ Stackless/unittests/test_defects.py
@@ -0,0 +1,82 @@
+import unittest
+import stackless
+
+"""
+Various regression tests for stackless defects.
+Typically, one can start by adding a test here, then fix it.
+Don't check in tests for un-fixed defects unless they are disabled (by adding a leading _)
+"""
+
+class TestTaskletDel(unittest.TestCase):
+    #Defect:  If a tasklet's tempval contains any non-trivial __del__ function, it will cause
+    #an assertion in debug mode due to violation of the stackless protocol.
+    #The return value of the tasklet's function is stored in the tasklet's tempval and cleared
+    #when the tasklet exits.
+    #Also, the tasklet itself would have problems with a __del__ method.
+    
+    class ObjWithDel:
+        def __del__(self):
+            self.called_func()
+        def called_func(self):
+            pass #destructor must call a function
+
+    class TaskletWithDel(stackless.tasklet):
+        def __del__(self):
+            self.func()
+        def func(self):
+            pass
+
+    class TaskletWithDelAndImport(stackless.tasklet):
+        def __del__(self):
+            import ctypes
+            
+    #Test that a tasklet tempval's __del__ operator works.
+    def testTempval(self):
+        def TaskletFunc(self):
+            return self.ObjWithDel()
+        
+        stackless.tasklet(TaskletFunc)(self)
+        stackless.run()
+    
+    #Test that a tasklet's __del__ operator works.
+    def testTasklet(self):
+        def TaskletFunc(self):
+            pass
+
+        self.TaskletWithDel(TaskletFunc)(self)
+        stackless.run()
+
+    #This test crashes.  Something strange happens, needs work
+    #an import in the __del__ causes strange things.
+    def _testTasklet2(self):
+        def TaskletFunc(self):
+            pass
+
+        self.TaskletWithDelAndImport(TaskletFunc)(self)
+        stackless.run()
+
+class Schedule(unittest.TestCase):
+    def testScheduleRemove(self):
+        #schedule remove doesn't work if it is the only tasklet running under watchdog
+        def func(self):
+            stackless.schedule_remove()
+            self.fail("We shouldn't be here")
+        stackless.run() #flush all runnables
+        stackless.tasklet(func)(self)
+        stackless.run()
+
+    def testScheduleRemove2(self):
+        #schedule remove doesn't work if it is the only tasklet with main blocked
+            #main tasklet is blocked, this should raise an error
+            self.assertRaises(RuntimeError, stackless.schedule_remove)
+            chan.send(None)
+        stackless.run() #flush all runnables
+        chan = stackless.channel()
+        stackless.tasklet(func)(self, chan)
+        chan.receive()
+
+if __name__ == '__main__':
+    import sys
+    if not sys.argv[1:]:
+        sys.argv.append('-v')
+    unittest.main()
